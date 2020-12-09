2d1a054744cc4f8c2caa2e4e952a844851765f94
Chad Coleman <chadcoleman1234@gmail.com>
2020-12-08 18:31:47 -0800

    finishing touches and final push from main PC

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# employeeManagment
+# Employee Management ![MIT License](https://img.shields.io/badge/License-MIT-Green)
+
+## Description
+
+This project can manage an employee database that includes categories for departments and roles. Employees, roles and departments can be added, updated, and viewed via the terminal.
+
+## Table of Contents
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contributions](#contributions)
+* [Testing](#testing)
+* [Credits](#credits)
+* [License](#license)
+
+## Installation
+Simply clone the repository
+
+## Usage
+Run 'node index.js' in the 'employee-manage' directory. You will be presented with the options to add, view, or update various items in the database. Schema and seed files are included to populate the database initially.
+
+NOTE: For the purpose of development, all data relevant to the server was kept private and will need to be set up in lines 7-11 of the index.js file prior to usage.
+
+## Contributions
+No contributions are currently accepted
+
+## Testing
+No testing is currently being done
+
+## Credits
+GitHub: https://www.github.com/chadalac27
+Email: chadcoleman1234@gmail.com
+
+## License
+MIT License: A short and simple permissive license with conditions only requiring preservation of copyright and license notices. Licensed works, modifications, and larger works may be distributed under different terms and without source code.
---
+++ index.js
@@ -0,0 +1,179 @@
+const inquirer = require("inquirer");
+const mysql = require("mysql");
+const cTable = require("console.table");
+require("dotenv").config();
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB,
+});
+connection.connect((err) => {
+  if (err) throw err;
+  console.log("Connected!");
+  init();
+});
+
+async function init() {
+  const { action } = await inquirer.prompt({
+    type: "list",
+    name: "action",
+    message: "How would you like to interact with the employee database?",
+    choices: [
+      "Add new entries",
+      "View existing entries",
+      "Update existing entries",
+      "Exit database",
+    ],
+  });
+  if (action == "Add new entries") {
+    await addEntries();
+  } else if (action == "View existing entries") {
+    await viewEntries();
+  } else if (action == "Update existing entries") {
+    updateEntries();
+  } else {
+    connection.end();
+  }
+}
+
+async function addEntries() {
+  // Determines what to add
+  const { category } = await inquirer.prompt({
+    type: "list",
+    name: "category",
+    message: "Would you like to add a department, role, or employee?",
+    choices: ["Department", "Role", "Employee"],
+  });
+  // Necessary question regardless of response
+  const { name } = await inquirer.prompt({
+    type: "input",
+    name: "name",
+    message: `What is the full name of this new ${category}?`,
+  });
+  if (category == "Department") {
+    // No department-specific questions must be asked, so the query is immediately made
+    connection.query("INSERT INTO department SET ?", { title: name }, (err) => {
+      if (err) throw err;
+    });
+  } else if (category == "Role") {
+    // Additional questions are necessary, so those are asked before the query is made
+    const { salary } = await inquirer.prompt({
+      type: "number",
+      name: "salary",
+      message: "What is the default salary of this role?",
+    });
+    const { department_id } = await inquirer.prompt({
+      type: "number",
+      name: "department_id",
+      message: "What is the department ID of this role?",
+    });
+
+    connection.query(
+      "INSERT INTO roles SET ?",
+      { title: name, salary: salary, department_id: department_id },
+      (err) => {
+        if (err) throw err;
+      }
+    );
+  } else {
+    // Additional questions are necessary, so those are asked before the query is made
+    let [first_name, ...last_name] = name.split(" ");
+    last_name = last_name.join(" ");
+    const { role_id } = await inquirer.prompt({
+      type: "number",
+      name: "role_id",
+      message: "What is the role ID of this employee's position?",
+    });
+    const { manager_id } = await inquirer.prompt({
+      type: "number",
+      name: "manager_id",
+      message: "What is the ID of the manager in charge of this employee?",
+    });
+    connection.query(
+      "INSERT INTO employee SET ?",
+      {
+        first_name: first_name,
+        last_name: last_name,
+        role_id: role_id,
+        manager_id: manager_id,
+      },
+      (err) => {
+        if (err) throw err;
+      }
+    );
+  }
+  console.log(`Successfully created new ${name} ${category}!`);
+  init();
+}
+
+async function viewEntries() {
+  const { category } = await inquirer.prompt({
+    type: "list",
+    name: "category",
+    message: "Which category would you like to view the entries of?",
+    choices: ["Departments", "Roles", "Employees", "Combined"],
+  });
+  switch (category) {
+    case "Departments":
+      connection.query("SELECT * FROM department", (err, res) => {
+        if (err) throw err;
+        console.table(res);
+      });
+      break;
+    case "Roles":
+      connection.query("SELECT * FROM roles", (err, res) => {
+        if (err) throw err;
+        console.table(res);
+      });
+      break;
+    case "Employees":
+      connection.query("SELECT * FROM employee", (err, res) => {
+        if (err) throw err;
+        console.table(res);
+      });
+      break;
+    case "Combined":
+      connection.query(
+        "SELECT employee.last_name, employee.first_name, roles.title, department.department, roles.salary FROM ((department INNER JOIN roles ON department.id = roles.department_id) INNER JOIN employee ON employee.role_id = roles.id)",
+        (err, res) => {
+          if (err) throw err;
+          console.table(res);
+        }
+      );
+  }
+  setTimeout(init, 2500);
+}
+
+function updateEntries() {
+  let names = [];
+  connection.query(
+    "SELECT employee.first_name FROM employee",
+    async (err, res) => {
+      if (err) throw err;
+      res.forEach((element) => {
+        names.push(element.first_name);
+      });
+      const { employee } = await inquirer.prompt({
+        type: "list",
+        name: "employee",
+        message: "Which employee's role is to be changed?",
+        choices: names,
+      });
+      const { newRole } = await inquirer.prompt({
+        type: "number",
+        name: "newRole",
+        message: `What is the new role ID that is to be assigned to ${employee}?`,
+      });
+      connection.query(
+        `UPDATE employee SET role_id = ${newRole} WHERE first_name = '${employee}'`, (err) => {
+          if (err) throw err;
+          console.log("Database successfully updated!");
+          init();
+        }
+      );
+    }
+  );
+}
